575a5396aacfaf0f187902d5eb0f7f40b04c9ce4
ChrisAchinga <achinga.chris@gmail.com>
2020-11-09 15:52:34 +0300

    created api-tester

---
+++ Timetable.js
@@ -0,0 +1,34 @@
+const timetable = [
+  {
+    subject: "maths",
+    time: "12pm",
+    lecturer: "knickson",
+    room: 102,
+  },
+  {
+    subject: "english",
+    time: "5am",
+    lecturer: "benson",
+    room: 222,
+  },
+  {
+    subject: "mechanics",
+    time: "4pm",
+    lecturer: "hoter",
+    room: 002,
+  },
+  {
+    subject: "business",
+    time: "10am",
+    lecturer: "edwin",
+    room: 212,
+  },
+  {
+    subject: "maths",
+    time: "9am",
+    lecturer: "knickson",
+    room: 102,
+  },
+];
+
+module.exports = timetable;
---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+const timetable = require('./Timetable');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+// api
+app.get('/api/timetable', (req, res) => {
+    res.json(timetable)
+})
+
+// set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});

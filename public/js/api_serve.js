91c202be6dabe5ec1b2613cbcb02e207677abf18
Timothy-Wangwe <wangwetymothy@gmail.com>
2020-11-12 09:08:06 +0300

    Filter 2.0.2

--- public/js/api_serve.js
+++ public/js/api_serve.js
@@ -26,49 +26,50 @@ fetch("http://127.0.0.1:3000/api/timetable")
     var campus = document.getElementById('campus');
     var start = document.getElementById('start_time');
     var end = document.getElementById('end_time');
-    var subject_dis = document.getElementById('subject_dis');
-    var campus_dis = document.getElementById('campus_dis');
-    var start_dis = document.getElementById('start_dis');
-    var end_dis = document.getElementById('end_dis');
+
+    // Display Results
+    function show_table() {
+      let table = document.getElementById("my-table");
+      table.innerHTML = '';
+      for (var i = 0; i < 50; i++) {
+        var row = `<tr class="table-row">
+                      <td id="time">${filtered_data[i].course_info[0].start_time}  ${filtered_data[i].course_info[0].end_time}</td>
+                      <td id="class">${filtered_data[i].className}</td>
+                      <td id="subject">${filtered_data[i].subject}</td>
+                      <td id="info">${filtered_data[i].course_info[0].campus}</td>
+                      <td id="catalog">${filtered_data[i].catalog_description}</td>
+                </tr>`;
+        table.innerHTML += row;
+      }
+    }
 
     // Filter by Subject
     subject.onchange = function () {
       filtered_data = data.filter(subj => subj.subject == subject.value);
-      subject_dis.innerHTML = subject.value;
       console.log(filtered_data);
+      show_table();
     }
 
     // Filter by Campus
     campus.onchange = function () {
-      filtered_data = filtered_data.filter(camp => camp.course_info[0].campus == campus.value);
-      campus_dis.innerHTML = campus.value;
+      filtered_data = data.filter(camp => camp.course_info[0].campus == campus.value);
       console.log(filtered_data);
+      show_table();
     }
 
     // Filter by Start Time
     start.onchange = function () {
-      filtered_data = filtered_data.filter(sta => sta.course_info[0].start_time == start.value);
-      start_dis.innerHTML = start.value;
+      filtered_data = data.filter(sta => sta.course_info[0].start_time == start.value);
       console.log(filtered_data);
+      show_table();
     }
 
     // Filter by End Time
     end.onchange = function () {
-      filtered_data = filtered_data.filter(en => en.course_info[0].end_time == end.value);
-      end_dis.innerHTML = end.value;
+      filtered_data = data.filter(en => en.course_info[0].end_time == end.value);
       console.log(filtered_data);
+      show_table();
 
-      let table = document.getElementById("my-table");
-      for (var i = 0; i < 50; i++) {
-        var row = `<tr class="table-row">
-                        <td id="time">${filtered_data[i].course_info[0].start_time}  ${filtered_data[i].course_info[0].end_time}</td>
-                        <td id="class">${filtered_data[i].className}</td>
-                        <td id="subject">${filtered_data[i].subject}</td>
-                        <td id="info">${filtered_data[i].course_info[0].campus}</td>
-                        <td id="catalog">${filtered_data[i].catalog_description}</td>
-                  </tr>`;
-        table.innerHTML += row;
-      }
     }
 
   }).catch(function (error) {

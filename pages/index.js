736c796ac8b9e4f494fbdb02ebee59fe4595731a
ChrisAchinga <achinga.chris@gmail.com>
2021-06-09 16:48:29 +0300

    fetch md files

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import fs from 'fs'
+import path from 'path'
 import Layout from '@/layouts/Layout'
 
 export default function Home() {
@@ -6,4 +8,15 @@ export default function Home() {
       <h1>Hello World</h1>
     </Layout>
   )
+}
+
+// get data from /post folder
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join('posts'))
+
+  console.log(files)
+
+  return {
+    props: {},
+  }
 }

a9e8083aa63772e97191a46e58bb6f554f6a2bb0
ChrisAchinga <achinga.chris@gmail.com>
2021-06-10 17:08:29 +0300

    gray-matter

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import fs from 'fs'
 import path from 'path'
+import matter from 'gray-matter'
 import Layout from '@/layouts/Layout'
 
-export default function Home() {
+export default function Home({ posts }) {
+  console.log(posts)
   return (
     <Layout>
       <h1>Hello World</h1>
@@ -13,10 +15,28 @@ export default function Home() {
 // get data from /post folder
 export async function getStaticProps() {
   const files = fs.readdirSync(path.join('posts'))
+  // console.log(files)
+  const posts = files.map((filename) => {
+    const slug = filename.replace('.md', '')
 
-  console.log(files)
+    const markdownWithMeta = fs.readFileSync(
+      path.join('posts', filename),
+      'utf-8'
+    )
+    // console.log(markdownWithMeta)
+    const { data: frontmatter } = matter(markdownWithMeta)
+
+    return {
+      slug,
+      frontmatter,
+    }
+  })
+
+  // console.log(posts)
 
   return {
-    props: {},
+    props: {
+      posts,
+    },
   }
 }
